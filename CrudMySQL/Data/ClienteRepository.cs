f0214e52448bc364ec45b308f0189c76c846e6da
gmand <gmand@DESKTOP-2KO8PEB>
2025-07-26 09:04:32 -0400

    GetAll

--- CrudMySQL/Data/ClienteRepository.cs
+++ CrudMySQL/Data/ClienteRepository.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using CrudMySQL.Models;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CrudMySQL.Data
 {
-    internal class ClienteRepository
+    public class ClienteRepository
     {
+        public List<Cliente> GetAll()
+        {
+            List<Cliente> clientes = new List<Cliente>();
+            string query = "SELECT Id, Nome, Email, Telefone, DataCadastro FROM Clientes";
+
+            using (MySqlConnection connection = Database.GetConnection())
+            {
+                try
+                {
+                    connection.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                clientes.Add(new Cliente
+                                {
+                                    Id = reader.GetInt32("Id"),
+                                    Nome = reader.GetString("Nome"),
+                                    Email = reader.IsDBNull("Email") ? null : reader.GetString("Email"),
+                                    Telefone = reader.IsDBNull("Telefone") ? null : reader.GetString("Telefone"),
+                                    DataCadastro = reader.GetDateTime("DataCadastro")
+                                });
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Erro ao obter clientes: " + ex.Message);
+                }
+            }
+
+            return clientes;
+        }
     }
 }
