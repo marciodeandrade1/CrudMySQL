f8483044b3e2cdf6be359b2e8e70a307aa562fde
gmand <gmand@DESKTOP-2KO8PEB>
2025-07-26 09:28:10 -0400

    Ajuste porta ConnectionString

--- CrudMySQL/Data/Database.cs
+++ CrudMySQL/Data/Database.cs
@@ -5,7 +5,7 @@ namespace CrudMySQL.Data
 {
     public static class Database
     {
-        private static string connectionString = "Server=localhost;Database=CrudDemo;Uid=root;Pwd=suasenha;";
+        private static string connectionString = "Server=localhost;Port=3306;Database=CrudDemo;Uid=root;Pwd=suasenha;";
 
         public static MySqlConnection GetConnection()
         {
